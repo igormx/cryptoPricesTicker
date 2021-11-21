048a88e8be6a6a1541ea3e410c1fdb48b325730f
carlosn <nacianceno@gmail.com>
2021-11-20 22:14:53 -0600

    init

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var availableSymbols = []string{"BTC", "DOT", "SOL", "SHIB", "ETH", "THETA"}
+var actualPrices = make(map[string]float64)
+
+func main() {
+	ticker := time.NewTicker(1 * time.Second)
+	for t := range ticker.C {
+		updatePrice("BTC")
+		fmt.Println(t, "Princes: ", actualPrices)
+	}
+}
+
+func updatePrice(symbol string) bool {
+	if _, ok := actualPrices[symbol]; !ok {
+		actualPrices[symbol] = 5.000
+		return true
+	}
+
+	percentage := (rand.Float64() * (0.001 + 5.000)) + 0.001
+	operation := rand.Intn(1)
+	if operation == 0 {
+		fmt.Println("sube")
+		fmt.Println((percentage / 100.00))
+		actualPrices[symbol] += (actualPrices[symbol] * (percentage / 100.00))
+	} else {
+		fmt.Println("baja")
+		actualPrices[symbol] -= (actualPrices[symbol] * (percentage / 100.00))
+	}
+
+	return true
+}
