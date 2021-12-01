917afa15d31efed2ac152276f4fe39966107a73d
igormx <nacianceno@gmail.com>
2021-11-30 23:35:51 -0600

    mod

--- main.go
+++ main.go
@@ -8,33 +8,33 @@ import (
 
 var availableSymbols = []string{"BTC", "DOT", "SOL", "SHIB", "ETH", "THETA"}
 var actualPrices = make(map[string]float64)
+var pricesChannles = make(map[string](chan float64))
 
 func main() {
-	pricesChannel := make(chan )
+	go updatePrice("BTC")
+	go updatePrice("ETH")
+	
+	fmt.Println("The rest of my application can continue")
 
-	ticker := time.NewTicker(1 * time.Second)
-	for t := range ticker.C {
-		updatePrice("BTC")
-		fmt.Println(t, "Princes: ", actualPrices)
-	}
+	select {}
 }
 
-func updatePrice(symbol string) bool {
-	if _, ok := actualPrices[symbol]; !ok {
-		actualPrices[symbol] = 5.000
-		return true
-	}
+func updatePrice(symbol string) {
+	ticker := time.NewTicker(1 * time.Second)
+	for _ = range ticker.C {
+		if _, ok := actualPrices[symbol]; !ok {
+			actualPrices[symbol] = 5.000
+			fmt.Println(symbol, actualPrices[symbol]);
+			continue
+		}
 
-	percentage := (rand.Float64() * (0.001 + 5.000)) + 0.001
-	operation := rand.Intn(1)
-	if operation == 0 {
-		fmt.Println("sube")
-		fmt.Println((percentage / 100.00))
-		actualPrices[symbol] += (actualPrices[symbol] * (percentage / 100.00))
-	} else {
-		fmt.Println("baja")
-		actualPrices[symbol] -= (actualPrices[symbol] * (percentage / 100.00))
+		percentage := (rand.Float64() * (0.001 + 5.000)) + 0.001
+		operation := rand.Intn(1)
+		if operation == 0 {
+			actualPrices[symbol] += (actualPrices[symbol] * (percentage / 100.00))
+		} else {
+			actualPrices[symbol] -= (actualPrices[symbol] * (percentage / 100.00))
+		}
+		fmt.Println(symbol, actualPrices[symbol]);
 	}
-
-	return true
 }
